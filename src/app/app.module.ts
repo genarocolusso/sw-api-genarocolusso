dcd5ed5fe05b92d8212e8aef8fc70fc4e6e64658
Genaro Colusso <genarocolusso@gmail.com>
2018-06-16 16:28:53 -0300

    :sparkles:  Style, Html, services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { getmoviesService } from './services/getmovies.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [getmoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/getmovies.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core'; 
+import { Http,Headers, Response, RequestOptions, URLSearchParams  } from "@angular/http";
+// creation and utility methods
+import { Observable, Subject, pipe } from 'rxjs';
+// operators all come from `rxjs/operators`
+import { map, takeUntil, tap } from 'rxjs/operators';
+ 
+
+import { environment } from '../../environments/environment';
+const apiUrl = environment.apiUrl;
+
+@Injectable()
+export class getmoviesService {
+
+    constructor(private http: Http){}
+    
+    getMovie( ): Observable<any[]>{
+
+       
+        return this.http
+        .get(  apiUrl+'/films/')
+        .pipe(map((response: Response)=>{
+             console.log(response.json())
+            return response.json().results;
+        }))
+    }
+
+
+}
